b94c099d1b916336b9819f3659ff9e3702147d40
Igor <igor-alexey@hotmail.com>
2021-12-24 12:43:21 -0300

    refactor 10% 0.54
    Currently learning inheritance quick hacks
    
    Co-Authored-By: Technoo <71846381+wTechnoo@users.noreply.github.com>

--- nice_ui.h
+++ nice_ui.h
@@ -1,6 +1,6 @@
 /**
  * @file nice_ui.h
- * @author Igor Alexey Marengo (igor@vortex-dev.com)
+ * @author vortex-dev.com
  * @brief Nice is a single-header C++ library that allows you to create a UI for GLFW with a simple object oriented API.
  * @brief It is designed to be flexible, easy to use and easy to understand.
  * @version 1.0
@@ -27,199 +27,94 @@
 #include <iostream>
 #include <vector>
 
-/* Declarations */
-
-// Enums //
-
-enum class UI_ELEMENT_TYPE
-{
-    FRAME,
-    IMAGE,
-    BUTTON,
-    TEXT_LABEL,
-    TEXT_BOX,
-    CHECK_BOX,
-    RADIO_BUTTON
-};
-
-enum class UI_ELEMENT_STATE
-{
-    NORMAL,
-    HOVER,
-    PRESSED,
-    DISABLED
-};
-
-// Structs //
-
 struct Vector2
 {
     float x, y;
 };
 
-struct Vector4
-{
-    float x, y, z, w;
-};
-
 struct UDim
 {
-    float
-    Scale, // Scale of the parent container from 0 to 1
-    Pixels; // Pixels of the window from 0 to a maximum of 65,535
-};
-
-struct UDim2
-{
-    UDim X,Y;
-};
-
-struct ElementPack
-{
-    std::vector<Nice_Frame> Frames;
-    std::vector<Nice_Image> Images;
-};
-
-
-// Classes //
-
-class Nice_Frame
-{
-    public:
-        ElementPack Children;
-        std::string Name;
-        bool Visible;
-        int ZIndex;
-        UDim2 Position;
-        UDim2 Size;
-        Vector2 Anchor_Point;
-        Vector4 Background_Color;
-        UI_ELEMENT_STATE State;
-        Nice_Frame() {Name = "Frame"; Position = {0.0f,0.0f}; Size = {100.0f,100.0f}; Anchor_Point = {0.0f,0.0f}; Background_Color = {1.0f,1.0f,1.0f,0.0f};}
-        virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Frame*);
-        virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Image*);
-        virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Button*);
-        virtual ElementPack* GetChildren();
-};
-
-class Nice_Image
-{
-    ElementPack Children;
-    std::string Name;
-    bool Visible;
-    int ZIndex;
-    UDim2 Position;
-    UDim2 Size;
-    Vector2 Anchor_Point;
-    Vector4 Background_Color;
-    Vector4 Image_Color;
-    UI_ELEMENT_STATE State;
-    Nice_Image() {Name = "Image"; Position = {0.0f,0.0f}; Size = {100.0f,100.0f}; Anchor_Point = {0.0f,0.0f}; Background_Color = {1.0f,1.0f,1.0f,0.0f}; Image_Color = {1.0f,1.0f,1.0f,1.0f};}
-    virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Frame*);
-    virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Image*);
-    virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Button*);
-    virtual ElementPack* GetChildren();
-};
-
-class Nice_Button
-{
-    public:
-        ElementPack Children;
-        std::string Name;
-        bool Visible;
-        int ZIndex;
-        UDim2 Position;
-        UDim2 Size;
-        Vector2 Anchor_Point;
-        Vector4 Background_Color;
-        UI_ELEMENT_STATE State;
-        Nice_Button() {Name = "Button"; Position = {0.0f,0.0f}; Size = {100.0f,100.0f}; Anchor_Point = {0.0f,0.0f}; Background_Color = {1.0f,1.0f,1.0f,0.0f};}
-        virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Frame*);
-        virtual void AddElement(UI_ELEMENT_TYPE, std::string, Nice_Image*);
-        // Skipping AddElement(UI_ELEMENT_TYPE, std::string, Nice_Button*); since there is no reason to add a button in a button.
-        virtual ElementPack* GetChildren();
+    float Scale;
+    int Offset;
 };
 
 /**
- * @brief Nice_Context is a container for all UI elements and context-related for a single window
- * Nice_Context is a container for all UI elements and context-related funtions for a single window, it should be created using New_Context()
- * and Destroyed using context->Destroy();
-*/
-class Nice_Context
+ * @brief 
+ * 
+ */
+struct UDim2
 {
-    private:
-        std::vector<Nice_Frame> elements;
-
-    public:
-        GLFWwindow* window;
-        virtual Nice_Frame* AddFrame(std::string);
-        void Update(); // Process input and update all UI elements.
-        void Render(); // Render all UI elements.
-        void Destroy(); // Destroy this Nice_Context and all its elements.
+    Vector2 x,y;
 };
 
-// Functions //
 
-// Create a new Nice_Context in the list of contexts, and returns a pointer to the new context.
-Nice_Context* New_Context(GLFWwindow* = glfwGetCurrentContext());
+/* Forward Declarations */
 
-// Get the mouse position inside the window
-Vector2 Get_Mouse_Position_Relative_To_Window(GLFWwindow* = glfwGetCurrentContext());
+class Nice_Context;
+class Nice_Element;
+class Nice_Frame;
+class Nice_Image;
+class Nice_Button;
+class Nice_Label;
+class Nice_TextField;
 
 // Only one of these should be defined at a time.
 #if defined(NICE_UI_IMPLEMENTATION) ^ defined(NICE_UI_DECLARATIONS_ONLY)
 
 /* Implementation Code */ 
 
-std::vector<Nice_Context> Nice_Contexts;
+std::vector<Nice_Context*> Contexts;
 
-/**
- * @brief Create a new element inside the context.
- * Create a new element of specified type and name (which is not an unique identifier) inside the context.
- * @param UI_ELEMENT_TYPE
- * @param name 
- */
-Nice_Frame* Nice_Context::AddFrame(std::string name)
-{
-    // create a new frame in the elements vector with the name, and return it
-    Nice_Frame* frame = new Nice_Frame();
-    frame->Name = name;
-    elements.push_back(*frame);
-    return frame;
+class Nice_Context {
+    private:
+        std::vector<Nice_Element> Elements;
+    public:
+        Nice_Context()
+        {
+            Contexts.push_back(this);
+        }
+        void AddElement(Nice_Element* element)
+        {
+            // create a new element and then add it to the stack, and then return a pointer to it.
 
-}
+        }
 
-void Nice_Context::Update()
-{
+};
 
-}
+class Nice_Element {
+    private:
 
-void Nice_Context::Render()
-{
-    // Render all UI elements
+    public:
+        Nice_Element()
+        {
+            //
+        }
+        void AddElement(Nice_Element* element)
+        {
+            
+        }
 
-}
+};
 
-void Nice_Context::Destroy()
-{
-    delete this;
-}
+class Nice_Frame : Nice_Element {
 
-Nice_Context* New_Context(GLFWwindow* window)
-{
-    // Create new context directly inside the Nice_Contexts vector.
-    Nice_Contexts.push_back(Nice_Context());
-    Nice_Contexts.back().window = window;
-    return &Nice_Contexts.back();
-}
+};
 
-Vector2 Get_Mouse_Position_Relative_To_Window(GLFWwindow* window)
-{
-    // Get mouse position relative to the window using glfwGetCursorPos() and return it as a Vector2
-    double x,y;
-    glfwGetCursorPos(window,&x,&y);
-    return Vector2{(float)x,(float)y};
-}
+class Nice_Image : Nice_Frame {
+
+};
+
+class Nice_Button : Nice_Frame {
+
+};
+
+class Nice_Label : Nice_Frame {
+
+};
+
+class Nice_TextField : Nice_Label {
+
+};
 
 /* End of Implementation */
 
--- nice_ui_demo.cpp
+++ nice_ui_demo.cpp
@@ -20,28 +20,49 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     // Create a Nice_UI Context
-    Nice_Context* context = New_Context();
-    Nice_Frame* frame = context->AddFrame("Nice_UI Example");
+    Nice_Context* context = new Nice_Context();
+    Nice_Frame* frame, topbar;
+    Nice_Button* closebutton, togglebutton;
+    Nice_Label* title, content;
+    /*
+    context->AddElement("Nice_UI Window", &frame);
+    frame->AddElement("Topbar", &topbar);
+    topbar->AddElement("Close Button", &closebutton);
 
     // Design interface before loop
 
-    
+    frame->Anchor_Point = Vector2{0.5f, 0.5f}; // The default anchor point is at the top left corner of the frame, this centers it to the middle.
+    frame->Position = UDim2{0.5f, 0.0f, 0.5f, 0.0f}; // Screen centered
+    frame->Size = UDim2{0.5f, 0.0f, 0.8f, 0.0f}; //  size in Scale and offset(pixels). Offset decimals are discarded.
+    frame->Background_Color = Vector4{0.1f, 0.1f, 0.1f, 1.0f}; // Dark gray background (RGBA)
+
+    Nice_Button* button = frame->AddElement(UI_ELEMENT_TYPE::BUTTON);
+
+    button->Text = "Click me!";
+    button->Position = UDim2{0.0f, 0.0f, 0.0f, 0.0f}; // Top left corner
+    button->Size = UDim2(1.0f, 0.0f, 0.1f, 0.0f); // Full width, 10% height
+    button->Background_Color = Vector4{0.0f, 0.0f, 1.0f, 1.0f}; // Blue background (RGBA)*/
 
     while (!glfwWindowShouldClose(window))
     {
+        // resize viewport to match window size
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
         // Update UI
-        context->Update();
+        //context->Update();
         
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Render all UI elements
-        context->Render();
+        //context->Render()
 
         glfwSwapBuffers(window);    
 
         glfwPollEvents();
     }
-    context->Destroy();
+    //context->Destroy();
     glfwTerminate();
     return 0;
 }
